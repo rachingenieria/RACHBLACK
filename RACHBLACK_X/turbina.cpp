14a0f9f4ea73b1272c170dd55bd56524012b261e
rachingenieria <rachingenieria.x@gmail.com>
2023-09-09 21:42:57 -0500

    TURBINA PARA THORR BALCKX I

--- RACHBLACK_X/turbina.cpp
+++ RACHBLACK_X/turbina.cpp
@@ -5,10 +5,8 @@
 
 int pos = 0;    // variable to store the servo position
 
-//const int minDutyMicros = 1060; // 1060 µs
-//const int maxDutyMicros = 1860; // 1860 µs
-
 const int minDutyMicros = 1000; // 1060 µs
+const int medDutyMicros = 1500; // 1060 µs
 const int maxDutyMicros = 2000; // 1860 µs
 
 // 490HZ Mas periodo 2.04 ms  255
@@ -16,30 +14,17 @@ const int maxDutyMicros = 2000; // 1860 µs
 void Turbina_Init(int pint) 
 {
   pinMode(pint, OUTPUT);
-  /*
-  ledcSetup(0, 490, 8); // Configurar el canal 0 para una frecuencia de 490 Hz y resolución de 16 bits
+
+  ledcSetup(0, 50, 8); // Configurar el canal 0 para una frecuencia de 490 Hz y resolución de 8 bits
   ledcAttachPin(pint, 0); // Asociar el pin al canal 0
 
-  int dutyCycle = map(minDutyMicros, 0, 2040, 0, 255); // Mapear el rango a la resolución de 16 bits
+  int dutyCycle = map(medDutyMicros, 0, 20000, 0, 255); // Mapear el rango a la resolución de 8 bits
   ledcWrite(0, dutyCycle); // Configurar el ciclo de trabajo
   delay(2000);
-  dutyCycle = map(maxDutyMicros, 0, 2040, 0, 255); // Mapear el rango a la resolución de 16 bits
+  dutyCycle = map(minDutyMicros, 0, 20000, 0, 255); // Mapear el rango a la resolución de 8 bits
   ledcWrite(0, dutyCycle); // Configurar el ciclo de trabajo
   delay(2000);
-  dutyCycle = map(minDutyMicros, 0, 2040, 0, 255); // Mapear el rango a la resolución de 16 bits
-  ledcWrite(0, dutyCycle); // Configurar el ciclo de trabajo
-  */
- 
-  ledcSetup(0, 50, 8); // Configurar el canal 0 para una frecuencia de 490 Hz y resolución de 16 bits
-  ledcAttachPin(pint, 0); // Asociar el pin al canal 0
-
-  int dutyCycle = map(minDutyMicros, 0, 20000, 0, 255); // Mapear el rango a la resolución de 16 bits
-  ledcWrite(0, dutyCycle); // Configurar el ciclo de trabajo
-  delay(2000);
-  dutyCycle = map(maxDutyMicros, 0, 20000, 0, 255); // Mapear el rango a la resolución de 16 bits
-  ledcWrite(0, dutyCycle); // Configurar el ciclo de trabajo
-  delay(2000);
-  dutyCycle = map(minDutyMicros, 0, 20000, 0, 255); // Mapear el rango a la resolución de 16 bits
+  dutyCycle = map(medDutyMicros, 0, 20000, 0, 255); // Mapear el rango a la resolución de 8 bits
   ledcWrite(0, dutyCycle); // Configurar el ciclo de trabajo
  
 }
@@ -47,9 +32,9 @@ void Turbina_Init(int pint)
 
 void Turbina_set(int pos)
 {
-  int posd = map(pos, 0, 99, 0, 800); // Mapear el rango a la resolución de 16 bits
+  int posd = map(pos, 0, 99, 0, 500); // Mapear el rango a la resolución de 16 bits
 
-  int dutyCycle = map(minDutyMicros + posd, 0, 20000, 0, 255); // Mapear el rango a la resolución de 16 bits 
+  int dutyCycle = map(medDutyMicros - posd, 0, 20000, 0, 255); // Mapear el rango a la resolución de 16 bits 
   ledcWrite(0, dutyCycle); // Configurar el ciclo de trabajo
 
 }
